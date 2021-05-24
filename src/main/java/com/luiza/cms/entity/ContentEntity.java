4054f5f3b437314b433c2bcea9c168b75610709d
Luiza Madalina <luizam.bubatu@gmail.com>
2021-05-24 19:38:04 +0300

    Adding Spring Data JPA entity for the content table

---
+++ src/main/java/com/luiza/cms/entity/ContentEntity.java
@@ -0,0 +1,98 @@
+package com.luiza.cms.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "content")
+public class ContentEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "mime_type")
+    private String mimeType;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "parent_id")
+    private int parentId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+}
